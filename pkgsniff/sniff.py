38fe4c4bbd4d29b35eea52cbe26fcced2d7172ad
Veras Nt <d.verasnt@gmail.com>
2017-10-22 09:00:13 -0500

    finished decoding

--- pkgsniff/sniff.py
+++ pkgsniff/sniff.py
@@ -22,6 +22,25 @@ class IP(Structure):
             ("src", c_ulong),
             ("dst", c_ulong)
             ]
+    def __new__(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+
+        #map consts of the protocol to their respective names
+        self.protocol_map   =   {1:"ICMP", 6:"TCP", 17:"UDP"}
+
+        #Ip addresses legible to the human race ..
+        self.src_address    =   socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address    =   socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        #protocol legible to humans
+        try:
+            self.protocol   =   self,protocol_map[self.protocol_num]
+        except:
+            self.protocol   =   str(self.protocol_num)
+
+
 
 #create a pure socket and associate it to the public interface
 if os.name  ==   "nt":
@@ -39,11 +58,21 @@ sniffer.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
 if os.name  ==  "nt":
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-#reads a single package
-print sniffer.recvfrom(65565)
+try:
 
-#if we're i windows-like, disable the promiscuos mode
-if os.name  ==  "nt":
-    sniffer.ioctl(socket.SIO_RECVALL. socket.RECVALL_OFF)
+    #read a package
+    raw_buffer  =   sinnfer.recvfrom(65565)[0]
+
+    #create an ip header from the 20 first bytes of the buffer
+    ip_header   =   IP(raw_buffer[0:20])
+
+    #show the detected protocols
+    print"[#] Protocol: %s %s - > %s"% (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+
+except KeyboardInterrupt:
+
+    #if we're i windows-like, disable the promiscuos mode
+    if os.name  ==  "nt":
+        sniffer.ioctl(socket.SIO_RECVALL. socket.RECVALL_OFF)
 
 
