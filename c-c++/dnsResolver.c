4daf10ce6a846dd3dc85e02af5e8b56ac2d17777
Dalmo Veras Neto <verasnt@protonmail.com>
2018-09-03 15:47:48 -0300

    mark 1, we must improve

--- c-c++/dnsResolver.c
+++ c-c++/dnsResolver.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
 
 int main( int argc, char *argv[]){
 	char *target;
 	target = argv[1];
 	struct hostent *host;
-	host = gethostbyname(target);
-	printf("%s :::> %s\n", target, inet_ntoa(*((struct in_addr *)host->h_addr)));
-	return 0;
+	char *result;
+	char *word[50];
+	FILE *dict;
+	dict = fopen(argv[2],"r");
+	if(argc < 2){
+		printf("[*] ONE DNS RESOLVER TO RULE THEM ALL [*]\n You must provide an target and a file with your words\n ./dnsResolv target.com dict.txt");
+	}else{	
+		while(fscanf(dict, "%s", &word) != EOF){
+			result = (char *) strcat(word, target);
+			host = gethostbyname(result);
+			if(host != NULL){
+				printf("%s :::> %s\n", target, inet_ntoa(*((struct in_addr *)host->h_addr)));
+				return -1;
+			}
+		}
+	}
 }
+
