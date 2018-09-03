4c202fd69641540788b6e962df65e98f97bc0459
Dalmo Veras Neto <verasnt@protonmail.com>
2018-09-03 00:40:08 -0300

    added a cut -d in order to improve the result

--- bash/parseurl.sh
+++ bash/parseurl.sh
@@ -5,7 +5,7 @@ echo "PARSE URL - ONE PARSE TO RULE THEM ALL"
 echo "You should run: $0 <EXAMPLE.COM>"
 else
 wget -O target.html "$1"
-grep href= target.html | cut -d "/" -f3 | grep "\.com\|\.org" | sort -u > hosts.txt
+grep href= target.html | cut -d "/" -f3 | grep "\.com\|\.org"| cut -d'"' -f1 | sort -u > hosts.txt
 #for url in $(cat hosts);do host "$url";done
 while read -r i; do
   host $i |grep "has address" 
