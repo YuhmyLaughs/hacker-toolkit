c9ff8ba9071492991c03d7474c33442c34a463f2
Dalmo Veras Neto <verasnt@protonmail.com>
2018-09-04 13:34:41 -0300

    changed to get ip as an argv  param

---
+++ py/whiteBeltPortScanner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import socket,sys
+
+for p in range(1,65535):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	if s.connect_ex((sys.argv[1],p)) == 0:
+		print("Port ",p,"[OPEN]")
+		s.close()
