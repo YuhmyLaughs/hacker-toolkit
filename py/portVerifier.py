0afd8b7c9b84f0e57e4f5efd1bfa930bc3293f70
Dalmo Veras Neto <verasnt@protonmail.com>
2018-09-04 13:50:59 -0300

    added argv

--- py/portVerifier.py
+++ py/portVerifier.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
-import socket
+import socket,sys
 
-ip = raw_input("Type your ip :: ")
-port = int(raw_input("Type the port number :: "))
+"""
+./portVerifier.py <target> <port>
+"""
+
+#ip = raw_input("Type your ip :: ")
+#port = int(raw_input("Type the port number :: "))
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if mysocket.connect_ex((ip, port)):
+if mysocket.connect_ex((sys.argv[1], sys.argv[2])):
 	print("Oh.. it's open folks..")
 else:
 	print(":( It's closed..")
