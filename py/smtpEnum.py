9ff1ddc9762e770d1b0d670839555a761511b7f8
Dalmo Veras Neto <verasnt@protonmail.com>
2018-10-27 11:09:20 -0300

    added support to receive a file of users

--- py/smtpEnum.py
+++ py/smtpEnum.py
@@ -2,12 +2,14 @@
 import socket
 import sys
 if len(sys.argv) != 3:
-    print "Usage: python smtpEnum.py <IP> <user>"
+    print "Usage: python smtpEnum.py <IP> <user_list.txt>"
+    sys.exit(0)
+user_file = open(argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((sys.argv[1],25))
 banner = s.recv(1024)
 print banner
-
-s.send('VRFY '+ sys.argv[2]'+\r\n')
-answer = s.recv(1024)
-print answer
+for user in user_file:
+    s.send('VRFY '+ sys.argv[2]'+\r\n')
+    answer = s.recv(1024)
+    print answer
