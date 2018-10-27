153a21491737d9fff053979f9320499e8d7d90a4
Dalmo Veras Neto <verasnt@protonmail.com>
2018-10-27 11:10:17 -0300

    added .readlines()

--- py/smtpEnum.py
+++ py/smtpEnum.py
@@ -9,7 +9,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((sys.argv[1],25))
 banner = s.recv(1024)
 print banner
-for user in user_file:
+for user in user_file.readlines():
     s.send('VRFY '+ sys.argv[2]'+\r\n')
     answer = s.recv(1024)
     print answer
