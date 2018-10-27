a1f1aa0ab39421362feab8ac4d7ba5c575ed78b6
Dalmo Veras Neto <verasnt@protonmail.com>
2018-10-27 10:59:09 -0300

    added sys.argv

--- py/smtpEnum.py
+++ py/smtpEnum.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python2
 import socket
+import sys
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.0.13',25))
+s.connect((sys.argv[1],25))
 banner = s.recv(1024)
 print banner
 
-s.send('VRFY root\r\n')
+s.send('VRFY '+ sys.argv[2]'+\r\n')
 answer = s.recv(1024)
 print answer
