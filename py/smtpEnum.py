f09d6d2adac792b5d7dd6e7cf1466eadb0609a56
Dalmo Veras Neto <verasnt@protonmail.com>
2018-10-27 11:02:22 -0300

    sys.argv verification helper

--- py/smtpEnum.py
+++ py/smtpEnum.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2
 import socket
 import sys
-
+if len(sys.argv) != 3:
+    print "Usage: python smtpEnum.py <IP> <user>"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((sys.argv[1],25))
 banner = s.recv(1024)
